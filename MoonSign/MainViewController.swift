104401aa3ad13f986f647ec1c50cf677d6db9d63
Mac <mac@Macs-iMac-2.local>
2019-01-14 09:42:04 +0545

    fixed minor issues

--- MoonSign/MainViewController.swift
+++ MoonSign/MainViewController.swift
@@ -82,7 +82,7 @@ class MainViewController: UIViewController,UITextViewDelegate {
     @objc func keyboardWillShow(notification: NSNotification) {
         if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
             if self.view.frame.origin.y == 0 {
-                self.view.frame.origin.y -= keyboardSize.height + 40
+                self.view.frame.origin.y -= keyboardSize.height + 50
             }
         }
     }
